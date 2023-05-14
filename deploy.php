ae89c88a790ebec74860b25968b4589312ad08bd
Fathur <hi.fathur.rohman@gmail.com>
2023-05-14 17:36:14 +0700

    retry again

--- deploy.php
+++ deploy.php
@@ -10,9 +10,7 @@ set('repository', 'git@github.com:fathur/election-bot.git');
 // Hosts
 
 host('production')
-    ->set('hostname', 'ec2-108-137-1-118.ap-southeast-3.compute.amazonaws.com')
-    ->set('remote_user', 'ubuntu')
-    ->set('identity_file', '~/.ssh/prod.pem')
+    ->set('hostname', 'production')
     ->set('deploy_path', '/codes/election-bot-dep')
     ->set('http_user', 'ubuntu');
 
