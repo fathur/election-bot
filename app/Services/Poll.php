4084652d90088acb0756730bbd7fd154728ae7b1
Fathur <hi.fathur.rohman@gmail.com>
2023-05-14 10:25:32 +0700

    add log

--- app/Services/Poll.php
+++ app/Services/Poll.php
@@ -158,6 +158,10 @@ class Poll
         foreach ($response->data as $twitterTweet) {
 
             if (!$this->isPassTextFilter($twitterTweet->text)) {
+                Log::warning([
+                    "message" => "Not pass filter",
+                    "text" => $twitterTweet->text
+                ]);
                 continue;
             }
 
